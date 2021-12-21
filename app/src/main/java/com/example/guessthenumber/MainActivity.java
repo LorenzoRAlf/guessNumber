cb29664e21c8af83ceba7a9d666a655dff811417
Lorenzo Ruiz <lruizalfieri@gmail.com>
2021-12-21 04:56:03 +0100

    added camera and ranking funtionality

--- app/src/main/java/com/example/guessthenumber/MainActivity.java
+++ app/src/main/java/com/example/guessthenumber/MainActivity.java
@@ -1,26 +1,31 @@
 package com.example.guessthenumber;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
-
 import android.app.AlertDialog;
-import android.app.Dialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.Dictionary;
-import java.util.Enumeration;
-import java.util.Hashtable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    protected int randomNumber;
+    private int guesses = 0;
+    private String name;
+    private String fileName;
+    static int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,42 +39,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         generateRandomNumber();
+        guesses = 0;
         TextView textGuesses = findViewById(R.id.guesses);
         textGuesses.setText("Guesses: 0");
     }
 
-    protected int randomNumber;
-    private int guesses = 0;
-    private CharSequence text;
-    private CharSequence guessText;
-    //private static ArrayList<Record> records = new ArrayList<Record>();
-    private Hashtable<String, Integer> playerRecords = new Hashtable<String, Integer>();
 
     public void guess(View v) {
         guesses++;
-        text = "";
-        guessText = "Guesses: " + guesses;
+        CharSequence text;
+        CharSequence guessText = "Guesses: " + guesses;
 
         EditText editText = findViewById(R.id.numberEdit);
         TextView textGuesses = findViewById(R.id.guesses);
 
         textGuesses.setText(guessText);
         String temp = editText.getText().toString();
-        editText.setText("");
 
         int value = Integer.parseInt(temp);
 
         if (value == randomNumber) {
             text = "You got it right!!!" + randomNumber;
             showDialog();
-        }
-        else if (value > randomNumber) {
-            text = "Go lower!"+ randomNumber;
-        }
-        else  {
-            text = "Go higher!"+ randomNumber;
-        }
+        } else if (value > randomNumber) {
+            text = "Go lower!" + randomNumber;
 
+        } else {
+            text = "Go higher!" + randomNumber;
+
+        }
+        editText.setText("");
         Toast.makeText(MainActivity.this, text, Toast.LENGTH_LONG).show();
     }
 
@@ -83,33 +82,61 @@ public class MainActivity extends AppCompatActivity {
         final EditText input = new EditText(this);
         alert.setView(input);
 
-        alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                String name = input.getText().toString();
-                playerRecords.put(name, guesses);
-                guesses = 0;
-                switchToRecord();
+        alert.setPositiveButton("Ok", (dialog, whichButton) -> {
+            name = input.getText().toString();
+
+            if (name.isEmpty()) {
+                showDialog();
+            } else {
+                counter++;
+                fileName = counter + String.valueOf(randomNumber) +
+                        "pic.jpg";
+                dispatchTakePictureIntent();
+
             }
         });
 
-        alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int whichButton) {
-                TextView textGuesses = findViewById(R.id.guesses);
-                textGuesses.setText("Guesses: 0");
-                generateRandomNumber();
-                // Canceled.
-            }
+        alert.setNegativeButton("Cancel", (dialog, whichButton) -> {
+            TextView textGuesses = findViewById(R.id.guesses);
+            textGuesses.setText("Guesses: 0");
+            generateRandomNumber();
+            // Canceled.
         });
 
         alert.show();
     }
 
-    private void switchToRecord() {
-        Intent goToRecord = new Intent(this, RecordActivity.class);
-        goToRecord = goToRecord.putExtra("records", playerRecords);
-        startActivity(goToRecord);
+    protected File getFile() {
+        File path = getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return new File(path, fileName);
     }
 
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Create the File where the photo should go
+        File photoFile = getFile();
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+            Uri photoURI = FileProvider.getUriForFile(this,
+                    "com.example.guessthenumber",
+                    photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Intent i = new Intent(getApplicationContext(), RecordActivity.class);
+        i.putExtra("recordName", name);
+        i.putExtra("recordGuesses", guesses);
+        i.putExtra("fileName", fileName);
+        startActivity(i);
+
+    }
 
     public void generateRandomNumber() {
         randomNumber = (int)Math.floor(Math.random()*100+1);
--- app/src/main/java/com/example/guessthenumber/RecordActivity.java
+++ app/src/main/java/com/example/guessthenumber/RecordActivity.java
@@ -1,30 +1,91 @@
 package com.example.guessthenumber;
 
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.*;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Comparator;
 
-import java.io.Serializable;
-import java.util.Hashtable;
 
 public class RecordActivity extends AppCompatActivity {
+
+
+    static class Record {
+        public int guesses;
+        public String file;
+        public String name;
+
+        public Record(String _file, int _guesses, String _name ) {
+            file = _file;
+            guesses = _guesses;
+            name = _name;
+        }
+
+
+    }
+
+    static ArrayList<Record> records = new ArrayList<>();
+    ArrayAdapter<Record> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record);
 
-        Bundle extras= getIntent().getExtras();
-        Serializable records = extras.getSerializable("records");
+        String name = getIntent().getStringExtra("recordName");
+        int guesses = getIntent().getIntExtra("recordGuesses", 0);
+        String fileName = getIntent().getStringExtra("fileName");
+        records.add(new Record(fileName, guesses, name));
+
+        if (records.size()>1){
+
+            records.sort(Comparator.comparingInt(o -> o.guesses));
+
+        }
+
+        adapter = new ArrayAdapter<Record>( this, R.layout.list_item, records )
+        {
+            @Override
+            public View getView(int pos, View convertView, ViewGroup container)
+            {
+
+                if( convertView==null ) {
+                    convertView = getLayoutInflater().inflate(R.layout.list_item, container, false);
+                }
+
+                String fileName = getItem(pos).file;
+                String guessesNumber = Integer.toString(getItem(pos).guesses);
+                ((TextView) convertView.findViewById(R.id.name)).setText(getItem(pos).name);
+                ((TextView) convertView.findViewById(R.id.guesses)).setText(guessesNumber);
+                Uri fileUri = Uri.fromFile(getFile(fileName));
+                ((ImageView) convertView.findViewById(R.id.userImage)).setImageURI(fileUri);
+                return convertView;
+            }
+
+        };
+
+        ListView lv = findViewById(R.id.recordsView);
+        lv.setAdapter(adapter);
+
+
 
     }
 
-    public void goBack() {
+    public void goBack(View view) {
         finish();
     }
+
+    protected File getFile(String fileName){
+
+        File path = getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        return new File(path, fileName);
+    }
+
 }
